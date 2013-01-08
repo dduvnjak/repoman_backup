3497aec9734145f8f6411d21ee95bcc088655606
dduvnjak <dario@atlantbh.com>
2013-01-08 11:50:12 +0000

    Modified update script

--- update.sh
+++ update.sh
@@ -5,8 +5,12 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 while read repo; do
 	prefix=`echo $repo|awk '{print $1}'`
 	url=`echo $repo|awk '{print $2}'`
+	
 	if [[ -d ${DIR}/${prefix} ]]; then
-		echo "Prefix ${prefix} exists"
+		echo "Prefix ${prefix} exists."
+		echo "Perorming update...."
+		git --git-dir=${DIR}/.git --work-tree=${DIR} subtree pull   --prefix=${prefix} ${url} master
 	fi;
+
 done < ${DIR}/repolist
 
