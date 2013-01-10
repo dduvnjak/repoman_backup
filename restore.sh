3504989728123e4d6dffd20152e6372f1771d4d7
dduvnjak <dario@atlantbh.com>
2013-01-10 12:52:36 +0000

    Added branch creation and pushing to original repo.

--- restore.sh
+++ restore.sh
@@ -4,19 +4,25 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 prefix=${1}
 
 function usage {
-  echo "usage: restore.sh repo_prefix"
+  echo -e "usage: restore.sh repo_prefix\n"
 }
 
 # check number of arguments
 if [[ $# -eq 0 ]] || [[ $# -gt 1 ]]; then
-        err_msg="ERROR: Invalid number of arguments \n";
+        echo "ERROR: Invalid number of arguments";
         usage
         exit -1;
 fi;
 
-if [[ ! `grep ${prefix} ${DIR}/repolist` ]]; then
+url=`grep ${prefix} ${DIR}/repolist | awk '{print $2}'`
+
+if [[ ${url} == "" ]]; then
   echo "Repo ${prefix} not found!"
   exit -1
 fi;
 
-#git --git-dir=${DIR}/.git --work-tree=${DIR} subtree split --prefix=${prefix} --annotate="(${prefix})" -b ${prefix}
+git --git-dir=${DIR}/.git --work-tree=${DIR} subtree split --prefix=${prefix} --annotate="(${prefix})" -b ${prefix}
+
+git push ${url} ${prefix}:master
+
+git branch -d ${prefix}
