4561beccf426b8d7ae9cf1ee7739f90c6fc531ee
dduvnjak <dario@atlantbh.com>
2013-01-10 12:39:05 +0000

    Added argument checking

--- restore.sh
+++ restore.sh
@@ -3,6 +3,17 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 prefix=${1}
 
+function usage {
+  echo "usage: restore.sh repo_prefix"
+}
+
+# check number of arguments
+if [[ $# -eq 0 ]] && [[ $# -gt 1 ]]; then
+        err_msg="ERROR: Invalid number of arguments \n";
+        usage
+        exit -1;
+fi;
+
 if [[ ! `grep ${prefix} ${DIR}/repolist` ]]; then
   echo "Repo ${prefix} not found!"
   exit 1
